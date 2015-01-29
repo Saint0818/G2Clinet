f2c7bf7a3691ec27037e4941516b257b2d3cd1fc
kk0815463 <kk351857>
2015-01-29 18:00:10 +0800

    add walk and run

--- Assets/04.Scripts/Game/GameJoystick.cs
+++ Assets/04.Scripts/Game/GameJoystick.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class GameJoystick: MonoBehaviour {
 	public float MoveSpeed = 10;
-	public GameObject targetPlayer;
+	public PlayerBehaviour targetPlayer;
 
 	void OnEnable(){
-		EasyTouch.On_TouchDown += On_TouchDown;
 		EasyJoystick.On_JoystickMove += On_JoystickMove;	
 		EasyJoystick.On_JoystickMoveEnd += On_JoystickMoveEnd;
 	}
@@ -21,27 +20,13 @@ public class GameJoystick: MonoBehaviour {
 		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
 	}
 
-	void On_TouchDown(Gesture gesture){
-		//Manual mode
-	}
-
 	void On_JoystickMove( MovingJoystick move){
-//		UIGame.Get.Game.OnJoystickMove(move);
-
-		if (targetPlayer) {
-			float angle = move.Axis2Angle(true);
-			int a = 90;
-			Vector3 rotation = new Vector3(0, angle + a, 0);
-			targetPlayer.transform.rotation = Quaternion.Euler(rotation);
-			Vector3 translate = Vector3.forward * Time.deltaTime * MoveSpeed;
-			targetPlayer.transform.Translate(translate);	
-		}
+		targetPlayer.OnJoystickMove(move);
 	}
 	
 	void On_JoystickMoveEnd (MovingJoystick move)
 	{
-//		UIGame.Get.Game.OnJoystickMoveEnd(move);
-		Debug.Log("trun AiMode after 3s");
+		targetPlayer.OnJoystickMoveEnd (move);
 	}
 	
 }
--- Assets/04.Scripts/Game/PlayerBehaviour.cs
+++ Assets/04.Scripts/Game/PlayerBehaviour.cs
@@ -26,7 +26,8 @@ public enum PlayerState
     Shoot = 19,
     Pass2 = 20,
 	AlleyOop_Pass = 21,
-	AlleyOop_Dunk = 22
+	AlleyOop_Dunk = 22,
+	RunAndDefence = 23
 }
 
 public static class PlayerActions
@@ -55,22 +56,89 @@ public static class PlayerActions
     public const string Pass2 = "Pass2";
 }
 
-public struct TSkillCD
-{
-    public float MaxTime;
-    public float CurrentTimer;
-    public bool CanUse;
-}
-
-public class TSkillAttribute
-{
-	public int No;
-    public int Kind;
-    public float Value;
-    public float CDTime;
-}
-
 public class PlayerBehaviour : MonoBehaviour
 {
-    
+	public Animator Control;
+	public PlayerState crtState = PlayerState.Idle;
+	public float basicMoveSpeed = 1;
+	public float WalkSpeed = 0.4f;
+	public bool IsDefense = true;
+	public float curSpeed = 0;
+
+	void Awake()
+	{
+		Control = gameObject.GetComponent<Animator>();
+
+//		AnimatorStateInfo[] test = Control.
+	}
+
+	public void OnJoystickMove(MovingJoystick move)
+	{
+		curSpeed = Mathf.Abs(move.joystickAxis.y);
+		SetSpeed(curSpeed);
+
+		if(Mathf.Abs(move.joystickAxis.y)>0)
+			AniState(PlayerState.Run);
+
+//		if (Mathf.Abs(move.joystickAxis.y)>0 && Mathf.Abs(move.joystickAxis.y)<0.5)
+//			SetSpeed(move.joystickAxis.y);
+//		else if (Mathf.Abs(move.joystickAxis.y)>=0.5){
+//			if(IsDefense)
+//				AniState(PlayerState.RunAndDefence);
+//			else
+//				AniState(PlayerState.Run);
+//		}
+
+		Debug.Log("move : " + move.joystickAxis);
+		float angle = move.Axis2Angle(true);
+		int a = 90;
+		Vector3 rotation = new Vector3(0, angle + a, 0);
+		transform.rotation = Quaternion.Euler(rotation);
+		Vector3 translate = Vector3.forward * Time.deltaTime * curSpeed * 10;
+		transform.Translate(translate);	
+	}
+
+	public void OnJoystickMoveEnd(MovingJoystick move)
+	{
+		if(IsDefense)
+			AniState(PlayerState.Defence);
+		else
+			AniState(PlayerState.Idle);
+	}
+
+	public void AniState(PlayerState state)
+	{
+		CloseAllState();
+
+		switch (state) {
+			case PlayerState.Idle:
+//				CloseAllState();
+			break;
+			case PlayerState.Walk:
+				
+			break;
+			case PlayerState.Run:
+				Control.SetBool("IsRun", true);
+			break;
+			case PlayerState.Defence:
+				Control.SetBool("IsDefence", true);
+				break;
+			case PlayerState.RunAndDefence:
+				Control.SetBool("IsRun", true);
+				Control.SetBool("IsDefence", true);
+			break;
+		}
+	}
+
+	public void SetSpeed(float value)
+	{
+		Control.SetFloat("Speed", value);
+	}
+
+	private void CloseAllState()
+	{
+		Control.SetBool("IsRun", false);
+		Control.SetBool("IsDefence", false);
+	}
+
 }
