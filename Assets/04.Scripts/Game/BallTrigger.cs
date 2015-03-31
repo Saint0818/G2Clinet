6175fb3f825be002b4b8b5b9e2ef86da4c00781e
elvisnicemarket <elvisnicemarket@gmail.com>
2015-03-31 16:37:47 +0800

    modify const

--- Assets/04.Scripts/Game/BallTrigger.cs
+++ Assets/04.Scripts/Game/BallTrigger.cs
@@ -81,7 +81,7 @@ public class BallTrigger : MonoBehaviour
 
 		if(passing){
 			if (GameController.Get.Catcher) {
-				if (Vector3.Distance (SceneMgr.Get.RealBall.gameObject.transform.position, GameController.Get.Catcher.transform.position) > GameController.Get.PickBallDis){
+				if (Vector3.Distance (SceneMgr.Get.RealBall.gameObject.transform.position, GameController.Get.Catcher.transform.position) > GameConst.PickBallDistance){
 					ParentRigidbody.gameObject.transform.position = Vector3.Lerp(ParentRigidbody.gameObject.transform.position, GameController.Get.Catcher.transform.position, 0.1f);
 				}else{
 					GameController.Get.SetBall(GameController.Get.Catcher);
