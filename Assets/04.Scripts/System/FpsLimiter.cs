92cbf84446e41a2fbcbfdb369bd88d20569c7a20
neoxu <neoxuisme@gmail.com>
2015-04-02 15:50:20 +0800

    I3 FPS

--- Assets/04.Scripts/System/FpsLimiter.cs
+++ Assets/04.Scripts/System/FpsLimiter.cs
@@ -7,18 +7,51 @@ public class FpsLimiter : MonoBehaviour {
 	float theDeltaTime= 0.0F;
 	float curTime= 0.0F;
 	float timeTaken = 0.0F;
-	public int frameRate = 30;
+	public int FrameRate = 30;
+	public bool ShowFPS = false;
+
+	private  float updateInterval = 0.5F;
+	private string fpsText = "";
+	private float accum   = 0; // FPS accumulated over the interval
+	private int   frames  = 0; // Frames drawn over the interval
+	private float timeleft; // Left time for current interval
 
 	// Use this for initialization
 	void Start () {
-		Application.targetFrameRate = frameRate;
+		Application.targetFrameRate = FrameRate;
 		QualitySettings.vSyncCount = 1;
-		theDeltaTime = (1.0F /frameRate);
+		theDeltaTime = (1.0F /FrameRate);
 		oldTime = Time.realtimeSinceStartup;
+
+		timeleft = updateInterval;  
+	}
+
+	void OnGUI() {
+		if (ShowFPS) {
+			GUI.Label(new Rect(100, 0, 100, 36), fpsText);
+		}
+	}
+
+	void Update()
+	{
+		timeleft -= Time.deltaTime;
+		accum += Time.timeScale/Time.deltaTime;
+		++frames;
+		
+		// Interval ended - update GUI text and start new interval
+		if( timeleft <= 0.0 )
+		{
+			// display two fractional digits (f2 format)
+			float fps = accum/frames;
+			fpsText = System.String.Format("{0:F2} FPS",fps);
+
+			timeleft = updateInterval;
+			accum = 0.0F;
+			frames = 0;
+		}
 	}
 
 	// Update is called once per frame
-	
 	void LateUpdate () {
 		curTime = Time.realtimeSinceStartup;
 		timeTaken = (curTime - oldTime);
@@ -28,5 +61,4 @@ public class FpsLimiter : MonoBehaviour {
 		
 		oldTime = Time.realtimeSinceStartup;
 	}
-	
 }
