dccfb28a9633944cb17ed72a2ade91aa393b8923
Saint0818 <Saint@SaintdeiMac.local>
2015-04-10 16:07:22 +0800

    ModelManager Fix

--- Assets/04.Scripts/System/ModelManager.cs
+++ Assets/04.Scripts/System/ModelManager.cs
@@ -169,7 +169,6 @@ public class ModelManager : MonoBehaviour {
 
 		return PB;
 	}
-
 	public void SetAvatarTexture(GameObject Player, GameStruct.TAvatar Attr, int BodyPart, int ModelPart, int TexturePart) {
 		if (Player) {
 			string bodyNumber = (Attr.Body / 1000).ToString();
@@ -401,37 +400,36 @@ public class ModelManager : MonoBehaviour {
 						}
 					}
 				}
-				
-				if(ikPin == null) {
-					Transform tPin = result.transform.FindChild("Pin");
-					if(tPin == null) {
-						GameObject obj = new GameObject();
-						obj.name = "Pin";
-						obj.transform.parent = result.transform;
-						if(bodyNumber.Equals("0")) {
-							obj.transform.localPosition = new Vector3(0, 2.9f, 1.7f);
-						} else if(bodyNumber.Equals("1")) {
-							obj.transform.localPosition = new Vector3(0, 2.7f, 1.7f);
-						} else if(bodyNumber.Equals("2")) {
-							obj.transform.localPosition = new Vector3(0, 2, 1.7f);
+				if(isUseRig){
+					if(ikPin == null) {
+						Transform tPin = result.transform.FindChild("Pin");
+						if(tPin == null) {
+							GameObject obj = new GameObject();
+							obj.name = "Pin";
+							obj.transform.parent = result.transform;
+							if(bodyNumber.Equals("0")) {
+								obj.transform.localPosition = new Vector3(0, 2.9f, 1.7f);
+							} else if(bodyNumber.Equals("1")) {
+								obj.transform.localPosition = new Vector3(0, 2.7f, 1.7f);
+							} else if(bodyNumber.Equals("2")) {
+								obj.transform.localPosition = new Vector3(0, 2, 1.7f);
+							}
+							obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
 						}
-						obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
+					}
+					
+					if(ikAim == null) {
+						Transform tAimParent = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head");
+						Transform tAim = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head/Aim");
+						if(tAim == null) {
+							GameObject obj = new GameObject();
+							obj.name = "Aim";
+							obj.transform.parent = tAimParent;
+							obj.transform.localPosition = Vector3.zero;
+							obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
+						}
 					}
 				}
-				
-				if(ikAim == null) {
-					Transform tAimParent = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head");
-					Transform tAim = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head/Aim");
-					if(tAim == null) {
-						GameObject obj = new GameObject();
-						obj.name = "Aim";
-						obj.transform.parent = tAimParent;
-						obj.transform.localPosition = Vector3.zero;
-						obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
-					}
-				}
-
-
 
 				if (dummyBall != null)
 					dummyBall.transform.parent = result.transform;
@@ -513,47 +511,49 @@ public class ModelManager : MonoBehaviour {
 			}
 
 			//IK
-			Transform tAim1 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head/Aim");
-			AimIK aimIK = result.GetComponent<AimIK> ();
-			if(aimIK == null) 
-				aimIK = result.AddComponent<AimIK> ();
-			aimIK.solver.transform = tAim1; 
-			Transform tAimBone2 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1");
-			Transform tAimBone3 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck");
-			Transform tAimBone4 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head");
-			aimIK.solver.bones = new IKSolver.Bone[3];
-			aimIK.solver.bones[0] = new IKSolver.Bone();
-			aimIK.solver.bones[1] = new IKSolver.Bone();
-			aimIK.solver.bones[2] = new IKSolver.Bone();
-			aimIK.solver.bones[0].transform = tAimBone2;
-			aimIK.solver.bones[0].weight = 0.25f;
-			aimIK.solver.bones[1].transform = tAimBone3;
-			aimIK.solver.bones[1].weight = 0.5f;
-			aimIK.solver.bones[2].transform = tAimBone4;
-			aimIK.solver.bones[2].weight = 0.5f;
+			if(isUseRig) {
+				Transform tAim1 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head/Aim");
+				AimIK aimIK = result.GetComponent<AimIK> ();
+				if(aimIK == null) 
+					aimIK = result.AddComponent<AimIK> ();
+				aimIK.solver.transform = tAim1; 
+				Transform tAimBone2 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1");
+				Transform tAimBone3 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck");
+				Transform tAimBone4 = result.transform.FindChild("Bip01/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 Head");
+				aimIK.solver.bones = new IKSolver.Bone[3];
+				aimIK.solver.bones[0] = new IKSolver.Bone();
+				aimIK.solver.bones[1] = new IKSolver.Bone();
+				aimIK.solver.bones[2] = new IKSolver.Bone();
+				aimIK.solver.bones[0].transform = tAimBone2;
+				aimIK.solver.bones[0].weight = 0.25f;
+				aimIK.solver.bones[1].transform = tAimBone3;
+				aimIK.solver.bones[1].weight = 0.5f;
+				aimIK.solver.bones[2].transform = tAimBone4;
+				aimIK.solver.bones[2].weight = 0.5f;
 
-			RootMotion.FinalIK.RotationLimitHinge boneRotationLimit2 = tAimBone2.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitHinge>();
-			RootMotion.FinalIK.RotationLimitHinge boneRotationLimit3 = tAimBone3.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitHinge>();
-//			RootMotion.FinalIK.RotationLimitHinge boneRotationLimit4 = tAimBone4.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitHinge>();
-			RootMotion.FinalIK.RotationLimitAngle boneRotationLimit5 = tAimBone4.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitAngle>();
-			boneRotationLimit2.useLimits = true;
-			boneRotationLimit2.axis = new Vector3(1, 0, 0);
-			boneRotationLimit2.zeroAxisDisplayOffset = 180;
-			boneRotationLimit2.min = -20;
-			boneRotationLimit2.max = 20;
-			boneRotationLimit3.useLimits = true;
-			boneRotationLimit3.axis = new Vector3(1, 0, 0);
-			boneRotationLimit3.zeroAxisDisplayOffset = 180;
-			boneRotationLimit3.min = -20;
-			boneRotationLimit3.max = 20;
-//			boneRotationLimit4.useLimits = true;
-//			boneRotationLimit4.axis = new Vector3(1, 0, 0);
-//			boneRotationLimit4.zeroAxisDisplayOffset = 180;
-//			boneRotationLimit4.min = -45;
-//			boneRotationLimit4.max = 45;
-			boneRotationLimit5.axis = new Vector3(-1, 1, 0);
-			boneRotationLimit5.limit = 20;
-			boneRotationLimit5.twistLimit = 20;
+				RootMotion.FinalIK.RotationLimitHinge boneRotationLimit2 = tAimBone2.gameObject.GetComponent<RootMotion.FinalIK.RotationLimitHinge>();
+				if(!boneRotationLimit2 )
+					boneRotationLimit2 = tAimBone2.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitHinge>();
+				RootMotion.FinalIK.RotationLimitHinge boneRotationLimit3 = tAimBone3.gameObject.GetComponent<RootMotion.FinalIK.RotationLimitHinge>();
+				if(!boneRotationLimit3 )
+					boneRotationLimit3 = tAimBone3.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitHinge>();
+				RootMotion.FinalIK.RotationLimitAngle boneRotationLimit5 = tAimBone4.gameObject.GetComponent<RootMotion.FinalIK.RotationLimitAngle>();
+				if(!boneRotationLimit5 )
+					boneRotationLimit5 = tAimBone4.gameObject.AddComponent<RootMotion.FinalIK.RotationLimitAngle>();
+				boneRotationLimit2.useLimits = true;
+				boneRotationLimit2.axis = new Vector3(1, 0, 0);
+				boneRotationLimit2.zeroAxisDisplayOffset = 180;
+				boneRotationLimit2.min = -20;
+				boneRotationLimit2.max = 20;
+				boneRotationLimit3.useLimits = true;
+				boneRotationLimit3.axis = new Vector3(1, 0, 0);
+				boneRotationLimit3.zeroAxisDisplayOffset = 180;
+				boneRotationLimit3.min = -20;
+				boneRotationLimit3.max = 20;
+				boneRotationLimit5.axis = new Vector3(-1, 1, 0);
+				boneRotationLimit5.limit = 20;
+				boneRotationLimit5.twistLimit = 20;
+			}
 
 			
 			//rig
