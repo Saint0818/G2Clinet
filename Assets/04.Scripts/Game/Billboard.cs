1ff242d85d1b1c2bf5bca771dca5ed58a839e1a8
kk0815463 <kk351857>
2015-06-25 17:31:45 +0800

    rename

---
+++ Assets/04.Scripts/Game/Billboard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ECameraType
+{
+	UI,
+	Game
+}
+public class Billboard : MonoBehaviour {
+	private Camera referenceCamera;
+	private Transform mTransform;
+	public ECameraType TypeOfCamera = ECameraType.Game;
+
+	void Awake() {
+
+		if(TypeOfCamera == ECameraType.Game)
+			referenceCamera = CameraMgr.Get.CourtCamera;
+
+		mTransform = transform;
+	}
+
+	void Update(){
+		if (referenceCamera) {
+			mTransform.LookAt (transform.position + referenceCamera.transform.rotation * Vector3.forward,
+	         referenceCamera.transform.rotation * Vector3.up);
+		}
+	}
+
+	public Camera SetCamera{
+		get { return referenceCamera;}
+		set { referenceCamera = value;}
+	}
+}
