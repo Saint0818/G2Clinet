56d57751962a05ee91e2e1a4432478fc4246ef00
Chen <developerjoy@gmail.com>
2015-08-26 16:53:06 +0800

    創角 StyleView 1) 左邊的按鈕做成元件(頭髮、衣服、褲子、鞋子)。

---
+++ Assets/04.Scripts/UI/CreateRole/UICreateRolePartButton.cs
@@ -0,0 +1,52 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class UICreateRolePartButton : MonoBehaviour
+{
+    public UISprite Slider;
+
+    public const float ShowTime = 0.15f; // 單位: 秒.
+    private float mElapsedTime;
+    private bool mIsPlaying;
+
+    [UsedImplicitly]
+    private void Awake()
+    {
+    }
+
+    [UsedImplicitly]
+    private void Update()
+    {
+        if(!mIsPlaying)
+            return;
+
+        if(mElapsedTime >= ShowTime)
+        {
+            mIsPlaying = false;
+            Slider.fillAmount = 1;
+            return;
+        }
+
+        mElapsedTime += Time.deltaTime;
+        Slider.fillAmount = mElapsedTime / ShowTime;
+
+        Debug.LogFormat("ElapsedTime:{0}, FillAmount:{1}", mElapsedTime, Slider.fillAmount);
+    }
+
+    public void Play()
+    {
+        Slider.gameObject.SetActive(true);
+        Slider.fillAmount = 0;
+
+        mElapsedTime = 0;
+        mIsPlaying = true;
+    }
+
+    public void Hide()
+    {
+        Slider.gameObject.SetActive(false);
+
+        mIsPlaying = false;
+    }
+}
--- Assets/04.Scripts/UI/CreateRole/UICreateRoleStyleView.cs
+++ Assets/04.Scripts/UI/CreateRole/UICreateRoleStyleView.cs
@@ -6,12 +6,20 @@ public class UICreateRoleStyleView : MonoBehaviour
 {
     public GameObject Window;
     public Transform ModelPreview;
+    public UICreateRolePartButton HairButton;
+    public UICreateRolePartButton ClothButton;
+    public UICreateRolePartButton PantsButton;
+    public UICreateRolePartButton ShoesButton;
 
     private GameObject mModel;
 
     [UsedImplicitly]
     private void Start()
     {
+        HairButton.Play();
+        ClothButton.Hide();
+        PantsButton.Hide();
+        ShoesButton.Hide();
     }
 
     public void Show(EPlayerPostion pos)
@@ -26,6 +34,54 @@ public class UICreateRoleStyleView : MonoBehaviour
     public void Hide()
     {
         Window.SetActive(false);
+    }
+
+    public void OnHairClicked()
+    {
+        if(UIToggle.current.value)
+        {
+//            Debug.Log("OnHairClicked");
+            HairButton.Play();
+            ClothButton.Hide();
+            PantsButton.Hide();
+            ShoesButton.Hide();
+        }
+    }
+
+    public void OnClothClicked()
+    {
+        if(UIToggle.current.value)
+        {
+//            Debug.Log("OnClothClicked");
+            HairButton.Hide();
+            ClothButton.Play();
+            PantsButton.Hide();
+            ShoesButton.Hide();
+        }
+    }
+
+    public void OnPantsClicked()
+    {
+        if(UIToggle.current.value)
+        {
+//            Debug.Log("OnPantsClicked");
+            HairButton.Hide();
+            ClothButton.Hide();
+            PantsButton.Play();
+            ShoesButton.Hide();
+        }
+    }
+
+    public void OnShoesClicked()
+    {
+        if(UIToggle.current.value)
+        {
+//            Debug.Log("OnShoesClicked");
+            HairButton.Hide();
+            ClothButton.Hide();
+            PantsButton.Hide();
+            ShoesButton.Play();
+        }
     }
 
     public void OnBackClicked()
