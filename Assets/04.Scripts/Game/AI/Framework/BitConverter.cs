6f1991b6dd4fcca9b96254dc11659f7795624962
Chen <developerjoy@gmail.com>
2015-09-07 14:57:35 +0800

    加入將字串轉成 bit array 的工具類別。

---
+++ Assets/04.Scripts/Game/AI/Framework/BitConverter.cs
@@ -0,0 +1,40 @@
+﻿
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace AI
+{
+    public class BitConverter
+    {
+        /// <summary>
+        /// 有限制的原因是不希望因為使用者的錯誤輸入, 造成一次 new 太大的記憶體.
+        /// </summary>
+        private const int MaxBitsNum = 50;
+
+        [CanBeNull]
+        public static bool[] Convert(string bitString)
+        {
+            if(bitString.Length >= MaxBitsNum)
+            {
+                Debug.LogErrorFormat("Parameter is too long, Length:{0}", bitString.Length);
+                return null;
+            }
+
+            bool[] bits = new bool[bitString.Length];
+            int[] ints = new int[bitString.Length];
+            for(int i = 0; i < bitString.Length; ++i)
+            {
+                if(!int.TryParse(bitString[bitString.Length - 1 - i].ToString(), out ints[i]))
+                {
+                    Debug.LogErrorFormat("Parse bool fail. char:{0}, index:{1}", bitString[i], i);
+                    return null;
+                }
+
+                bits[i] = System.Convert.ToBoolean(ints[i]);
+            }
+
+            return bits;
+        }
+    }
+} // end of the namespace AI
+
