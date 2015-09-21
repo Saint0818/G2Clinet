890456b9447c23939f456b1253eeeb3052012cf3
Chen <developerjoy@gmail.com>
2015-09-21 18:43:44 +0800

    離開 PositionView 的時候，關掉特效。

--- Assets/04.Scripts/UI/CreateRole/UI3DCreateRolePositionView.cs
+++ Assets/04.Scripts/UI/CreateRole/UI3DCreateRolePositionView.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 using JetBrains.Annotations;
 
 /// <summary>
-/// 搭配 UICreateRolePositionView 一起使用的介面, 專門負責 3D 場景內的互動操作.
+/// 搭配 UICreateRolePositionView 一起使用的介面, 專門負責球員的互動操作.
 /// </summary>
+/// <remarks>
+/// 使用方法:
+/// <list type="number">
+/// <item> 用 UI3DCreateRole.Get.PositionView 取得 instance. </item>
+/// <item> Call Show() or Hide() 控制球員要不要顯示. </item>
+/// <item> Call Select() 通知哪位球員被選擇. </item>
+/// </list>
+/// </remarks>
 public class UI3DCreateRolePositionView : MonoBehaviour
 {
     public Transform CenterParent;
@@ -28,16 +36,22 @@ public class UI3DCreateRolePositionView : MonoBehaviour
 
         public bool Visible
         {
-            set { mModel.SetActive(value); }
+            set
+            {
+                mModel.SetActive(value);
+                mShadow.SetActive(value);
+            }
         }
 
         private readonly GameObject mModel;
+        private readonly GameObject mShadow;
         private readonly int mPlayerID;
         private readonly Animator mAnimator;
 
-        public Player(Transform parent, string name, int playerID)
+        public Player(Transform parent, GameObject shadow, string name, int playerID)
         {
             mPlayerID = playerID;
+            mShadow = shadow;
 
             mModel = UICreateRole.CreateModel(parent, name, mPlayerID,
                         CreateRoleDataMgr.Ins.GetBody(EPlayerPostion.G)[0],
@@ -57,14 +71,6 @@ public class UI3DCreateRolePositionView : MonoBehaviour
     }
 
     private readonly Dictionary<EPlayerPostion, Transform> mParents = new Dictionary<EPlayerPostion, Transform>();
-//
-//    private readonly Dictionary<EPlayerPostion, GameObject> mModels = new Dictionary<EPlayerPostion, GameObject>();
-//    private readonly Dictionary<EPlayerPostion, int> mPlayerIDs = new Dictionary<EPlayerPostion, int>
-//    {
-//        {EPlayerPostion.G, 1},
-//        {EPlayerPostion.F, 2},
-//        {EPlayerPostion.C, 3}
-//    };
 
     private readonly Dictionary<EPlayerPostion, Player> mPlayers = new Dictionary<EPlayerPostion, Player>();
         
@@ -76,38 +82,9 @@ public class UI3DCreateRolePositionView : MonoBehaviour
         mParents.Add(EPlayerPostion.C, CenterParent);
 
         // 現在的版本是讓玩家可以選擇 ID: 1, 2, 3 的角色.
-//        var obj = UICreateRole.CreateModel(GuardParent, "GuardModel", 1,
-//            CreateRoleDataMgr.Ins.GetBody(EPlayerPostion.G)[0],
-//            CreateRoleDataMgr.Ins.GetHairs(EPlayerPostion.G)[0],
-//            CreateRoleDataMgr.Ins.GetCloths(EPlayerPostion.G)[0],
-//            CreateRoleDataMgr.Ins.GetPants(EPlayerPostion.G)[0],
-//            CreateRoleDataMgr.Ins.GetShoes(EPlayerPostion.G)[0]);
-//        obj.AddComponent<SelectEvent>(); // 避免發生 Error.
-//        mModels.Add(EPlayerPostion.G, obj);
-
-        mPlayers.Add(EPlayerPostion.G, new Player(GuardParent, "Guard", 1));
-
-        //        obj = UICreateRole.CreateModel(ForwardParent, "ForwardModel", 2,
-        //            CreateRoleDataMgr.Ins.GetBody(EPlayerPostion.F)[0],
-        //            CreateRoleDataMgr.Ins.GetHairs(EPlayerPostion.F)[0],
-        //            CreateRoleDataMgr.Ins.GetCloths(EPlayerPostion.F)[0],
-        //            CreateRoleDataMgr.Ins.GetPants(EPlayerPostion.F)[0],
-        //            CreateRoleDataMgr.Ins.GetShoes(EPlayerPostion.F)[0]);
-        //        obj.AddComponent<SelectEvent>(); // 避免發生 Error.
-        //        mModels.Add(EPlayerPostion.F, obj);
-
-        mPlayers.Add(EPlayerPostion.F, new Player(ForwardParent, "Forward", 2));
-
-        //        obj = UICreateRole.CreateModel(CenterParent, "CenterModel", 3,
-        //            CreateRoleDataMgr.Ins.GetBody(EPlayerPostion.C)[0],
-        //            CreateRoleDataMgr.Ins.GetHairs(EPlayerPostion.C)[0],
-        //            CreateRoleDataMgr.Ins.GetCloths(EPlayerPostion.C)[0],
-        //            CreateRoleDataMgr.Ins.GetPants(EPlayerPostion.C)[0],
-        //            CreateRoleDataMgr.Ins.GetShoes(EPlayerPostion.C)[0]);
-        //        obj.AddComponent<SelectEvent>(); // 避免發生 Error.
-        //        mModels.Add(EPlayerPostion.C, obj);
-
-        mPlayers.Add(EPlayerPostion.C, new Player(CenterParent, "Center", 3));
+        mPlayers.Add(EPlayerPostion.G, new Player(GuardParent, GuardShadow, "Guard", 1));
+        mPlayers.Add(EPlayerPostion.F, new Player(ForwardParent, ForwardShadow, "Forward", 2));
+        mPlayers.Add(EPlayerPostion.C, new Player(CenterParent, CenterShadow, "Center", 3));
     }
 
     public void Show()
@@ -118,9 +95,7 @@ public class UI3DCreateRolePositionView : MonoBehaviour
             pair.Value.PlayAnimation();
         }
 
-        CenterShadow.SetActive(true);
-        ForwardShadow.SetActive(true);
-        GuardShadow.SetActive(true);
+        SelectSFX.gameObject.SetActive(true);
     }
 
     public void Hide()
@@ -130,9 +105,7 @@ public class UI3DCreateRolePositionView : MonoBehaviour
             pair.Value.Visible = false;
         }
 
-        CenterShadow.SetActive(false);
-        ForwardShadow.SetActive(false);
-        GuardShadow.SetActive(false);
+        SelectSFX.gameObject.SetActive(false);
     }
 
     public void Select(EPlayerPostion pos)
