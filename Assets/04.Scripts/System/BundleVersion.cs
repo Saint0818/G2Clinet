779fc73c57b1d7d82c5c8199e4ea6fb8f8568f1e
kk0815463 <kk351857>
2015-12-02 18:38:56 +0800

    version

--- Assets/04.Scripts/System/BundleVersion.cs
+++ Assets/04.Scripts/System/BundleVersion.cs
@@ -1,5 +1,5 @@
 public static class BundleVersion
 {
-	public static readonly float Version = 0.171f;
+	public static readonly float Version = 0.181f;
 }
 
--- Assets/Editor/GameEditor/GEBuildTool.cs
+++ Assets/Editor/GameEditor/GEBuildTool.cs
@@ -7,8 +7,8 @@ using System.Collections.Generic;
 using System;
 
 public class GEBuildTool : GEBase {
-	public float mVersion = 0.171f;
-    public int mVersionCode = 17;
+	public float mVersion = 0.181f;
+    public int mVersionCode = 18;
     public string mPass = "csharp2014";
     public string mPath;
     public int HeadItemIndex = 51;
