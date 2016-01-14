1b9b376be7f2424778ba59b85de703dfe12a19fc
Saint0818 <Saint@SaintdeiMac.local>
2016-01-14 20:45:23 +0800

    modify UIITemHint

--- Assets/04.Scripts/UI/Hint/HintInlayView.cs
+++ Assets/04.Scripts/UI/Hint/HintInlayView.cs
@@ -9,6 +9,7 @@ public class HintInlayView : MonoBehaviour {
 	public UISprite QualityOctagon;
 	public UISprite ItemPic;
 	public UILabel AmountLabel;
+	public UILabel[] KindLabels;
 
     private void Awake()
 	{
@@ -30,12 +31,20 @@ public class HintInlayView : MonoBehaviour {
 	
 	public void UpdateUI(TItemData itemData)
 	{
+		if(KindLabels == null || KindLabels.Length ==0 ) {
+			KindLabels = new UILabel[AttrKindsIcon.Length];
+			for (int i=0; i<AttrKindsIcon.Length; i++){
+				KindLabels[i] = AttrKindsIcon[i].transform.FindChild("KindLabel").GetComponent<UILabel>();
+			}
+		}
+
 		QualityOctagon.spriteName = "Patch" + Mathf.Clamp(itemData.Quality, 1, 5).ToString();
 		
 		for(int i=0; i < itemData.Bonus.Length; i++)
         {
 			AttrKindsIcon[i].gameObject.SetActive(true);
 			AttrKindsIcon[i].spriteName = "AttrKind_" + itemData.Bonus[i].GetHashCode();
+			KindLabels[i].text = TextConst.S(10500 + itemData.Bonus[i].GetHashCode());
 		}
 		
 		for (int i=0; i<itemData.BonusValues.Length; i++) {
