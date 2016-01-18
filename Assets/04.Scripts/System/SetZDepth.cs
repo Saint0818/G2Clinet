aca6b84e7a32e4368e51f4b88515271269e3187c
kk0815463 <kk351857>
2016-01-18 15:55:05 +0800

    add z depth

---
+++ Assets/04.Scripts/System/SetZDepth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Effects/SetZDepth")]
+[RequireComponent(typeof(Renderer))]
+
+public class SetZDepth : MonoBehaviour
+{
+    private int basic = 3000;
+    public int queue = 1;
+
+    public int[] queues;
+
+    protected void Start()
+    {
+        if (!GetComponent<Renderer>() || !GetComponent<Renderer>().sharedMaterial || queues == null)
+            return;
+        GetComponent<Renderer>().sharedMaterial.renderQueue = basic + queue;
+        for (int i = 0; i < queues.Length && i < GetComponent<Renderer>().sharedMaterials.Length; i++)
+            GetComponent<Renderer>().sharedMaterials[i].renderQueue = queues[i];
+    }
+}
+
