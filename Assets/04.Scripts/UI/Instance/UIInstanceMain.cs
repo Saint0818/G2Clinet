aaa5d78845da0878c914b34c7523cc3f07ae39a1
Chen <developerjoy@gmail.com>
2016-01-28 15:23:58 +0800

    加入副本章節移動按鈕。

--- Assets/04.Scripts/UI/Instance/UIInstanceMain.cs
+++ Assets/04.Scripts/UI/Instance/UIInstanceMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,11 @@ using UnityEngine;
 /// </summary>
 /// 使用方法:
 /// <list type="number">
-/// <item> 用 ClearAllChapters, AddChapter, SelectChapter 控制章節. </item>
+/// <item> 用 ClearAllChapters, AddChapter 控制有哪些章節. </item>
 /// <item> ShowChapters or ShowStages 控制要顯示章節 or 關卡. </item>
-/// <item> Call SelectStage 控制 ScrollView 捲動到哪個關卡. </item>
+/// <item> Call SelectChapter 控制 ScrollView 顯示哪個章節. </item>
+/// <item> Call MoveToChapter 控制 ScrollView 捲動到哪個章節. </item>
+/// <item> Call SelectStage 控制 ScrollView 顯示哪個關卡. </item>
 /// </list>
 public class UIInstanceMain : MonoBehaviour
 {
@@ -21,8 +24,13 @@ public class UIInstanceMain : MonoBehaviour
     public GameObject StageView;
     public UIButton StageBackButton;
 
+    public UIButton PreviousChapterButton;
+    public UIButton NextChapterButton;
+
     public UIScrollView ChapterScrollView;
     public UIScrollView StageScrollView;
+
+    public GameObject FullScreenBlock;
 
     /// <summary>
     /// <para> 呼叫時機: 關卡的 Start 按鈕按下. </para>
@@ -36,12 +44,22 @@ public class UIInstanceMain : MonoBehaviour
     private const float ChapterInterval = 900;
     private const float StageInterval = 230;
 
+    /// <summary>
+    /// 幾個 Frame, ScrollView 捲動完畢.
+    /// </summary>
+    private const int MoveStep = 10;
+
     private readonly Dictionary<int, UIInstanceChapter> mChapters = new Dictionary<int, UIInstanceChapter>();
     private readonly List<UIInstanceStage> mStages = new List<UIInstanceStage>();
 
     private void Start()
     {
         StageBackButton.onClick.Add(new EventDelegate(ShowChapters));
+
+        NextChapterButton.onClick.Add(new EventDelegate(() => MoveToChapter(CurrentChapter + 1)));
+        PreviousChapterButton.onClick.Add(new EventDelegate(() => MoveToChapter(CurrentChapter - 1)));
+
+        FullScreenBlock.SetActive(false);
     }
 
     public void ClearAllChapters()
@@ -69,15 +87,10 @@ public class UIInstanceMain : MonoBehaviour
 
     public void SelectChapter(int chapter)
     {
-        var reviseChapter = chapter;
-        if(chapter < 1)
-            reviseChapter = 1;
-        else if(chapter > mChapters.Count)
-            reviseChapter = mChapters.Count;
+        var reviseChapter = getReviseChapter(chapter);
 
         // 其實顯示某個章節, 只是移動一整個章節的寬度. 第1章的位置是 (0),
         // 第 2 章的位置是 (-900), 所以這邊才會這樣計算.
-//        Vector3 targetPos = new Vector3((reviseChapter - 1) * -ChapterInterval, 0, 0);
         Vector3 targetPos = getChapterTargetPos(reviseChapter);
         Vector3 moveAmount = targetPos - ChapterScrollView.transform.localPosition;
         
@@ -86,6 +99,61 @@ public class UIInstanceMain : MonoBehaviour
 
 //        Debug.LogFormat("ScrollView TargetPos:{0}, MoveAmount:{1}, Pos:{2}", 
 //            targetPos, moveAmount, ScrollView.transform.localPosition);
+    }
+
+    private int getReviseChapter(int chapter)
+    {
+        if(chapter < 1)
+            return 1;
+        if(chapter > mChapters.Count)
+            return mChapters.Count;
+        return chapter;
+    }
+
+    public void MoveToChapter(int chapter)
+    {
+        var reviseChapter = getReviseChapter(chapter);
+
+        Vector3 targetPos = getChapterTargetPos(reviseChapter);
+        Vector3 moveAmount = targetPos - ChapterScrollView.transform.localPosition;
+
+//        Debug.LogFormat("MoveToChapter, Chapter:{0}, TargetPos:{1}, MoveAmount:{2}", reviseChapter, targetPos, moveAmount);
+
+        StartCoroutine(moveChapter(moveAmount));
+    }
+
+    private IEnumerator moveChapter(Vector3 moveAmount)
+    {
+        FullScreenBlock.SetActive(true);
+        ChapterScrollView.GetComponent<UICenterOnChild>().enabled = false;
+        
+        Vector3 stepMoveAmount = moveAmount / MoveStep;
+
+//        Debug.LogFormat("moveChapter, stepMoveAmount:{0}", stepMoveAmount);
+
+        for(int i = 0; i < MoveStep; i++)
+        {
+            ChapterScrollView.MoveRelative(stepMoveAmount);
+            yield return new WaitForEndOfFrame();
+        }
+
+        ChapterScrollView.GetComponent<UICenterOnChild>().enabled = true;
+        FullScreenBlock.SetActive(false);
+    }
+
+    /// <summary>
+    /// 目前介面顯示哪一個章節. 1: 第一章, 2: 第二章.
+    /// </summary>
+    public int CurrentChapter
+    {
+        get
+        {
+            // 其實這部份只是根據 ScrollView 的 Local Position 來判斷現在顯示哪一章.
+            // 當位置是 0 時, 表示是選擇第 1 章; 當位置是 (-900) 時, 表示是選擇第 2 章;
+            // 當位置是 (-1800) 時, 表示是選擇第 3 章. 以此類推.(目前章節的寬度是 900)
+            // +1 也是依據算式而做的調整, 不然明明是第 1 章, 但是卻回傳 0.
+            return (int)Mathf.Abs(ChapterScrollView.transform.localPosition.x / ChapterInterval) + 1;
+        }
     }
 
     private Vector3 getChapterTargetPos(int chapter)
