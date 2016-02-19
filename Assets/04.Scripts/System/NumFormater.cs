da586bfd672ae86f617adb9364cac72e1b21be96
Chen <developerjoy@gmail.com>
2016-02-19 14:22:37 +0800

    修正數值 0，得到的是空字串。

--- Assets/04.Scripts/System/NumFormater.cs
+++ Assets/04.Scripts/System/NumFormater.cs
@@ -7,6 +7,6 @@ public static class NumFormater
     public static string Convert(int num)
     {
         // https://msdn.microsoft.com/zh-tw/library/0c899ak8%28v=vs.110%29.aspx
-        return String.Format(CultureInfo.InvariantCulture, "{0:#,#}", num);
+        return String.Format(CultureInfo.InvariantCulture, "{0:#,0}", num);
     }
 }
