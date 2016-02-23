6b52d5202f6ed1e17a239552c92be747b1ce497e
Chen <developerjoy@gmail.com>
2016-02-23 14:16:01 +0800

    1) 加上登入獎勵介面控制器。 2) 更新字串表。

---
+++ Assets/04.Scripts/UI/DailyLogin/UIDailyLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+/// <summary>
+/// 副本介面.
+/// </summary>
+/// <remarks>
+/// 使用方法:
+/// <list type="number">
+/// <item> 用 Get 取得 instance. </item>
+/// <item> Call Show() 顯示副本. </item>
+/// <item> Call Hide() 關閉副本. </item>
+/// </list>
+/// </remarks>
+[DisallowMultipleComponent]
+public class UIDailyLogin : UIBase
+{
+    private static UIDailyLogin instance;
+    private const string UIName = "UIDailyLogin";
+
+    private void Start()
+    {
+        
+    }
+
+    public bool Visible { get { return gameObject.activeSelf; } }
+
+    public void Show()
+    {
+        Show(true);
+    }
+
+    public void Hide()
+    {
+        RemoveUI(UIName);
+    }
+
+    public static UIDailyLogin Get
+    {
+        get
+        {
+            if(!instance)
+            {
+                UI2D.UIShow(true);
+                instance = LoadUI(UIName) as UIDailyLogin;
+            }
+			
+            return instance;
+        }
+    }
+}
