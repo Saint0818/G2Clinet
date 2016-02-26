cd154c0bcbae3ea295bdbbbe95a73ab13c1c2a30
Saint0818 <Saint@SaintdeiMac.local>
2016-02-26 15:37:13 +0800

    modify push

--- Assets/04.Scripts/Game/PlayerBehaviour.cs
+++ Assets/04.Scripts/Game/PlayerBehaviour.cs
@@ -2715,7 +2715,7 @@ public class PlayerBehaviour : MonoBehaviour
                     animatorEvent.floatParameter = 1;
                     TimeScaleCallBack(animatorEvent, this);
 					
-					PlayerSkillController.ResetUseSkill();
+//					PlayerSkillController.ResetUseSkill();
                     if (isBlock)
                     {
                         if (GameController.Get.BallState == EBallState.CanBlock)
