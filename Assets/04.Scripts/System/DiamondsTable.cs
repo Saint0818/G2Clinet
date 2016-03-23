882c8247d6314a9d233e203bf8aa08cc8f23db37
Chen <developerjoy@gmail.com>
2016-03-23 16:17:26 +0800

    可讀取 Diamonds 表格。

---
+++ Assets/04.Scripts/System/DiamondsTable.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using GameEnum;
+using JetBrains.Annotations;
+using UnityEngine;
+
+/// <summary>
+/// 讀取 diamonds.json 的表格, 這個表格是記錄鑽石相關的資訊.
+/// </summary>
+/// 使用方法:
+/// <list type="number">
+/// <item> 用 Ins 取得 instance. </item>
+/// <item> Call GetXXX 取得資料; Call HasXXX 檢查資料. </item>
+/// </list>
+public class DiamondsTable
+{
+    private static readonly DiamondsTable INSTANCE = new DiamondsTable();
+    public static DiamondsTable Ins
+    {
+        get { return INSTANCE; }
+    }
+
+    private readonly Dictionary<TDiamondData.EKind, TDiamondData> mDiamonds = new Dictionary<TDiamondData.EKind, TDiamondData>();
+
+    private DiamondsTable() {}
+
+    public void Load(string jsonText)
+    {
+        clear();
+
+        // 刪除 ["] 字元.
+        jsonText = jsonText.Replace("\"[", "[");
+        jsonText = jsonText.Replace("]\"", "]");
+        jsonText = jsonText.Replace("\"{", "{");
+        jsonText = jsonText.Replace("}\"", "}");
+
+        var diamonds = JsonConvertWrapper.DeserializeObject<TDiamondData[]>(jsonText);
+        foreach(TDiamondData element in diamonds)
+        {
+            mDiamonds.Add(element.Kind, element);
+        }
+
+        Debug.Log("[diamonds parsed finished.] ");
+    }
+
+    private void clear()
+    {
+        mDiamonds.Clear();
+    }
+
+    public bool Has(TDiamondData.EKind kind)
+    {
+        return mDiamonds.ContainsKey(kind);
+    }
+
+    [CanBeNull]
+    public TDiamondData Get(TDiamondData.EKind kind)
+    {
+        if(Has(kind))
+            return mDiamonds[kind];
+        return null;
+    }
+}
---
+++ Assets/04.Scripts/System/TDiamondData.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+
+/// <summary>
+/// Diamonds 表格中的某一筆資料.
+/// </summary>
+public class TDiamondData
+{
+    public enum EKind
+    {
+        Unknown = 0,
+        ResetDailyChallenge = 1
+    }
+
+    public int[] Nums
+    {
+        get { return mNums ?? (mNums = new [] {Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8}); }
+    }
+
+    private int[] mNums;
+
+    [UsedImplicitly]
+    public EKind Kind { get; private set; }
+
+    [UsedImplicitly]
+    public int Num1 { get; private set; }
+    [UsedImplicitly]
+    public int Num2 { get; private set; }
+    [UsedImplicitly]
+    public int Num3 { get; private set; }
+    [UsedImplicitly]
+    public int Num4 { get; private set; }
+    [UsedImplicitly]
+    public int Num5 { get; private set; }
+    [UsedImplicitly]
+    public int Num6 { get; private set; }
+    [UsedImplicitly]
+    public int Num7 { get; private set; }
+    [UsedImplicitly]
+    public int Num8 { get; private set; }
+}
