57f55f209ceb0c52062c8bdf33d69ad1cbcc9364
Saint0818 <Saint@SaintdeiMac.local>
2016-04-19 16:59:13 +0800

    modify UIInGameMission

--- Assets/04.Scripts/UI/UIInGameMissionView.cs
+++ Assets/04.Scripts/UI/UIInGameMissionView.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class UIInGameMissionView : MonoBehaviour {
 
@@ -6,6 +7,11 @@ public class UIInGameMissionView : MonoBehaviour {
 	public bool IsFinish;
 	public Animator FinishAnimator;
 	public UILabel DescLabel;
+	public GameObject AddTarget;
+
+	void OnEnable() {
+		AddTarget.SetActive(false);
+	}
 
 	public void Show()
 	{
@@ -22,11 +28,20 @@ public class UIInGameMissionView : MonoBehaviour {
 
 	public void UpdateUI(string desc)
 	{
+		if(!DescLabel.text.Equals (desc))
+			StartCoroutine(targetInvisible ());
 		DescLabel.text = desc;
+
 	}
 
 	public void UpdateFin () {
 		IsFinish = true;
 		FinishAnimator.SetTrigger("Finish");
+	}
+		
+	IEnumerator targetInvisible () {
+		AddTarget.SetActive(true);
+		yield return new WaitForSeconds(0.5f);
+		AddTarget.SetActive(false);
 	}
 }
