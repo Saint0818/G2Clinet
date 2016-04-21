ce57f376d0e7f097bd9627199b6b3bd9d1fe2567
Saint0818 <Saint@SaintdeiMac.local>
2016-04-21 17:56:54 +0800

    更新OverallHint

--- Assets/04.Scripts/UI/UIOverallHint.cs
+++ Assets/04.Scripts/UI/UIOverallHint.cs
@@ -5,7 +5,16 @@ public class UIOverallHint : UIBase {
 	private static UIOverallHint instance = null;
 	private const string UIName = "UIOverallHint";
 
+	private Animator animator;
 	private UILabel overallHint;
+	private GameObject[] goArrow = new GameObject[2];
+
+	private float showTime = 3;
+	private float waitTime = 0.5f;
+	private float ranTime = 0.5f;
+	private float tempAValue;
+
+	private bool isClose = true;
 
 	public static bool Visible {
 		get {
@@ -27,6 +36,14 @@ public class UIOverallHint : UIBase {
 		}
 	}
 
+	public static void UIShow(bool isShow){
+		if (instance)
+			instance.Show(isShow);
+		else
+			if (isShow) 						
+				Get.Show(isShow);
+	}
+
 	public static UIOverallHint Get
 	{
 		get {
@@ -37,17 +54,77 @@ public class UIOverallHint : UIBase {
 		}
 	}
 
+	void FixedUpdate () {
+		if(showTime > 0) {
+			showTime -= Time.deltaTime;
+			if(showTime <= 0) 
+				UIShow(false);
+			else if(showTime <= 0.5f && !isClose) {
+				isClose = true;
+				animator.SetTrigger("Close");
+			}
+		}
+
+		if(waitTime > 0) {
+			waitTime -= Time.deltaTime;
+			if(waitTime <= 0)
+				waitTime = 0;
+		}
+
+		if(ranTime > 0 && waitTime <= 0) {
+			ranTime -= Time.deltaTime;
+			overallHint.text = string.Format("{0:00.000}", Random.Range(10f, 99f));
+			if(ranTime <= 0)
+				overallHint.text = tempAValue.ToString();
+		}
+	}
+
 	protected override void InitCom() {
+		animator = gameObject.GetComponent<Animator>();
 		overallHint = GameObject.Find(UIName + "/Window/Bottom/OverallView/OverallLabel").GetComponent<UILabel>();
-		//UIEventListener.Get(GameObject.Find(UIName + "/CoverCollider")).onClick = OnClose;
+		goArrow[0] = GameObject.Find(UIName + "/Window/UpArrow");
+		goArrow[1] = GameObject.Find(UIName + "/Window/DownArrow");
 	}
 
-	public void ShowView (string value ) {
+	private void hideArrow () {
+		for(int i=0; i<goArrow.Length; i++)
+			goArrow[i].SetActive(false);
+	}
+
+	private void showArrow (bool isUp) {
+		hideArrow ();
+		if(isUp)
+			goArrow[0].SetActive(true);
+		else
+			goArrow[1].SetActive(true);
+	}
+
+	public void ShowView (string value) {
 		Visible = true;
+		if(isClose) {
+			isClose = false;
+			animator.SetTrigger("Open");
+		}
 		overallHint.text = value;
+		showTime = 3;
+		ranTime = 0;
+		waitTime = 0;
+		hideArrow ();
 	}
 
-	public void OnClose (GameObject go) {
-		Visible = false;
+	//bValue之前數值  aValue之後變成的數值
+	public void ShowView (float bValue, float aValue) {
+		if(!Visible)
+			waitTime = 0.5f;
+		Visible = true;
+		if(isClose) {
+			isClose = false;
+			animator.SetTrigger("Open");
+		}
+		overallHint.text = bValue.ToString();
+		tempAValue = aValue;
+		showTime = 3;
+		ranTime = 0.5f;
+		showArrow(aValue >= bValue);
 	}
 }
