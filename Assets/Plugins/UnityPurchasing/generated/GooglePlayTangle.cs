85a8af23917e3c685f5712e8c112080ddb8a729c
neoxu <neoxuisme@gmail.com>
2016-05-04 09:59:35 +0800

    I26 google map key

--- Assets/Plugins/UnityPurchasing/generated/GooglePlayTangle.cs
+++ Assets/Plugins/UnityPurchasing/generated/GooglePlayTangle.cs
@@ -4,9 +4,9 @@
 namespace UnityEngine.Purchasing.Security {
     public class GooglePlayTangle
     {
-        private static byte[] data = System.Convert.FromBase64String("qywfMj9kXM530R1yqTirFekJjVp9z0xvfUBLRGfLBcu6QExMTEhNTvLBCRlR2PwBFPJ/LKrjdmevK47ptf4w9rPCFwXu3irw/yKdLHprmuUhF3IVg+mB5G3z6blx0Au+OvyMaPkkOFWp2hnn8kRpL7Fzdwe3Zz2YzKWo1WtVa5odb9eSJkZXYv8ljMQzmwUQxlaOfZa56LaMFZwq78KTPWjtFZWDQr925mn/5MYaFp7lOyA3evmHJMAJu8WvfqgtdercGEPlzcU8lTdSkYkkkE4+WU0MQF8nZhadus9MQk19z0xHT89MTE2eZpcUhy5oqNK+i6wTt7HHKSXn9L5FCCarC/K+OOBxx7gAbqSi3VLO300auRcANiEgKPjvYvJIQk9OTE1M");
-        private static int[] order = new int[] { 9,9,4,8,5,13,7,11,10,12,13,12,13,13,14 };
-        private static int key = 77;
+        private static byte[] data = System.Convert.FromBase64String("EJOdkqIQk5iQEJOTkhEH3A7B9JruVYWkW+1aRoBwsRh2Ye2+8PolBNu8hLSkdlZEkJOp2EGZQf1olEDZM7q1g8gdIXFAL82cioN0mdkwLuli+b3gTN/bDX/cZ7FWCAQPtHtRqvdh2d6KOlGnHFqXt8hP+lJd+TY55FXnhnfn6Oyl/kpyR2ByMdxoysOEBCpUtWNk3B054c5H5WXe0iKc9WD+YuvYzVDVfZUrtzKIRlWXLgztMJwYWGkdj60wMBr98vadVbQuTOiiEJOwop+Um7gU2hRln5OTk5eSkdAffr09I4Br8hIUOZXmqRIYViFdk4HHRaMpcZhsx6ngfrb0LF415uA0e4hnG7KmaUwVotxkzkC0FVC5/ngXPAqL7FZJVZCRk5KT");
+        private static int[] order = new int[] { 1,5,12,9,6,7,8,8,11,9,11,13,13,13,14 };
+        private static int key = 146;
 
         public static byte[] Data() {
             return Obfuscator.DeObfuscate(data, order, key);
